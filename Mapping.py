ca1298ee3296014d2d5f9cfdcaca39b86a631c0b
ZacharyArnott <zacharyarnott@gmail.com>
2016-11-07 14:59:47 -0800

    Create Mapping.py

---
+++ Mapping.py
@@ -0,0 +1,7 @@
+#Mapping Task Function
+
+,,,
+Mapping
+This task analyses the data from sensing and addes new data point to the map. This involves taking the raw data from the sensors and converting the data to a distance using the calibration parameters for each sensor. 
+Inputs:Raw sensor data "" 
+Outputs:Map aka updatable data structure of map.
